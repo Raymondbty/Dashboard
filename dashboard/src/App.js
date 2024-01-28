e93b1c7a05e57fcb4fa92dd322c5886f09f56c2c
amaury.bariety@epitech.eu <amaury.bariety@epitech.eu>
2024-01-28 21:57:46 +0100

    feat: add basic non-redirective auth discord

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./style.css";
 import "./styles.css";
 import Login from "./Login";
 import Register from "./Register";
-import Dashboard from "./Dashboard";  // Importez la nouvelle page Dashboard
+import Dashboard from "./Dashboard";
+import Discord from "./Discord";
 
 const App = () => {
+  const handleDiscordRedirect = (params) => {
+    return <Navigate to="/dashboard/discord" />;
+  };
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/discord" element={<Discord />} />
       </Routes>
     </Router>
   );
--- dashboard/src/Discord.js
+++ dashboard/src/Discord.js
@@ -3,10 +3,19 @@ import discordIcon from './discord.png';
 import './Discord.css';
 
 const Discord = () => {
+  const handleDiscordLogin = () => {
+    const clientId = '1170627038776934400';
+    const redirectUri = 'http://localhost:3000/discord';
+    const authUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify`;
+
+    window.location.href = authUrl;
+  };
+
   return (
     <div className="discord-service-container">
       <img src={discordIcon} alt="Discord Icon" />
       <h3>Discord</h3>
+      <button onClick={handleDiscordLogin}>Login with Discord</button>
     </div>
   );
 };
