c777db0cfa927ece4193c4f49ef1baff648462f3
amaury.bariety@epitech.eu <amaury.bariety@epitech.eu>
2024-02-01 15:10:37 +0100

    feat: restructure entire front, add weather widget/service

--- dashboard/src/Dashboard.js
+++ dashboard/src/Dashboard.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import WeatherService from './Weather';
-import DiscordService from './Discord';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import weatherIcon from './weather.png';
+import discordIcon from './discord.png';
+import './Dashboard.css';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const redirectToWeather = () => {
+    navigate('/weather');
+  };
+
+  const redirectToDiscord = () => {
+    navigate('/discord');
+  };
+
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <div className="services">
-        <Link to="/weather">
-          <WeatherService />
-        </Link>
-        <Link to="/discord">
-          <DiscordService />
-        </Link>
+    <div className="container">
+      <div className="item" onClick={redirectToWeather}>
+        <img src={weatherIcon} alt="Weather" width="100" height="100" />
+        <p>Weather</p>
+      </div>
+      <div className="item" onClick={redirectToDiscord}>
+        <img src={discordIcon} alt="Discord" width="100" height="100" />
+        <p>Discord</p>
       </div>
     </div>
   );
--- dashboard/src/Weather.js
+++ dashboard/src/Weather.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import weatherIcon from './weather.png';
 import './Weather.css';
 
 const Weather = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleGetWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ad6e12e12b001e5b3e58fc461af326d9`
+      );
+
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
   return (
     <div className="weather-service-container">
       <img src={weatherIcon} alt="Weather Icon" />
+      <div>
+        <input
+          type="text"
+          placeholder="Enter City"
+          value={city}
+          onChange={handleCityChange}
+        />
+        <button onClick={handleGetWeather}>Get Weather</button>
+      </div>
+      {weatherData && (
+        <div>
+          <h3>{weatherData.name}, {weatherData.sys.country}</h3>
+          <p>Temperature: {weatherData.main.temp} K</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+        </div>
+      )}
     </div>
   );
 };
