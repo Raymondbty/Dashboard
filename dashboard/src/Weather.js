d45fe7a056ba27d57bb3630fc58aad01a83e5eb0
amaury.bariety@epitech.eu <amaury.bariety@epitech.eu>
2024-02-02 22:03:36 +0100

    feat: add icons to weather

--- dashboard/src/Weather.js
+++ dashboard/src/Weather.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import weatherIcon from './weather.png';
-import './Weather.css';
 import { useAppContext } from './AppContext';
+import { FaSun, FaCloud, FaCloudRain, FaSnowflake, FaWind, FaMist } from "react-icons/fa";
+import { RiMistFill } from "react-icons/ri";
+import { CiCloudSun, CiCloud, CiCloudDrizzle, CiSun } from "react-icons/ci";
+import { FaRegSnowflake } from "react-icons/fa";
+import { LuWind } from "react-icons/lu";
+import './Weather.css';
 
 const Weather = () => {
   const { dispatch } = useAppContext();
@@ -30,20 +34,44 @@ const Weather = () => {
     }
   };
 
+  const getWeatherIcon = (weatherDescription) => {
+    let iconClassName = "weather-icons";
+  
+    switch (weatherDescription) {
+      case 'Clear':
+        iconClassName = "weather-icon-clear";
+        return <CiSun className={iconClassName} />;
+      case 'Clouds':
+        iconClassName = "weather-icon-clouds";
+        return <CiCloud className={iconClassName} />;
+      case 'Rain':
+        iconClassName = "weather-icon-rain";
+        return <CiCloudDrizzle className={iconClassName} />;
+      case 'Snow':
+        iconClassName = "weather-icon-snow";
+        return <FaRegSnowflake className={iconClassName} />;
+      case 'Wind':
+        iconClassName = "weather-icon-wind";
+        return <LuWind className={iconClassName} />;
+      case 'Mist':
+        iconClassName = "weather-icon-mist";
+        return <RiMistFill className={iconClassName} />;
+      case 'CloudSun':
+        return <CiCloudSun className={iconClassName} />;
+      default:
+        return null;
+    }
+  };
+  
   return (
     <div className="weather-service-container">
-      <img src={weatherIcon} alt="Weather Icon" />
-      <div>
-        <input
-          type="text"
-          placeholder="Enter City"
-          value={city}
-          onChange={handleCityChange}
-        />
+      {weatherData && getWeatherIcon(weatherData.weather[0].main)}
+      <div className="weather-input-container">
+        <input type="text" placeholder="Enter City" value={city} onChange={handleCityChange} />
         <button onClick={handleGetWeather}>Get Weather</button>
       </div>
       {weatherData && (
-        <div>
+        <div className="weather-data-container">
           <h3>{weatherData.name}, {weatherData.sys.country}</h3>
           <p>Temperature: {weatherData.main.temp} K</p>
           <p>Description: {weatherData.weather[0].description}</p>
