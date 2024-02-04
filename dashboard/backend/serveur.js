767fcb9bcfffc1f9a3ede8bdadf528c7674bb9f2
amaury.bariety@epitech.eu <amaury.bariety@epitech.eu>
2024-02-04 11:41:40 +0100

    fix: CORS MOUAHAHAHAHHAHA

--- dashboard/backend/serveur.js
+++ dashboard/backend/serveur.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Ajout de cors
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
+app.use(cors()); // Utilisation du middleware cors
 
 app.post('/register', (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  // todo
+    // todo
 
   res.status(200).json({ success: true, message: 'Registration successful' });
 });
 
 app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-// hard-code for test
-  
-    if (email === 'amaury.bariety@gmail.com' && password === 'password') {
-      res.status(200).json({ success: true, message: 'Login successful' });
-    } else {
-      res.status(401).json({ success: false, message: 'Login failed' });
-    }
+  const { email, password } = req.body;
+
+
+  if (email === 'amaury.bariety@gmail.com' && password === 'password') {
+    res.status(200).json({ success: true, message: 'Login successful' });
+  } else {
+    res.status(401).json({ success: false, message: 'Login failed' });
+  }
 });
 
 app.listen(port, () => {
