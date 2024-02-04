0034dc668cf0e7bcf83564081ab833447c86d3cf
amaury.bariety@epitech.eu <amaury.bariety@epitech.eu>
2024-02-04 11:39:34 +0100

    feat: add hard-coded paswd & user to test login

--- dashboard/backend/serveur.js
+++ dashboard/backend/serveur.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
 
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
+app.post('/register', (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  // todo
+
+  res.status(200).json({ success: true, message: 'Registration successful' });
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+// hard-code for test
+  
+    if (email === 'amaury.bariety@gmail.com' && password === 'password') {
+      res.status(200).json({ success: true, message: 'Login successful' });
+    } else {
+      res.status(401).json({ success: false, message: 'Login failed' });
+    }
 });
 
 app.listen(port, () => {
